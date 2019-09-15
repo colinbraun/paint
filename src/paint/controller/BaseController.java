8a748516eff3a951467ae9885eb358721362c349
electrolitic <electrolitic21@gmail.com>
2019-09-15 16:01:05 -0500

    Fix controller bug

--- src/paint/controller/BaseController.java
+++ src/paint/controller/BaseController.java
@@ -7,10 +7,6 @@ import java.util.ResourceBundle;
 
 public class BaseController implements Initializable {
 
-    private BaseController() {
-
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
